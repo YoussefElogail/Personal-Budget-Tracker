de6491e6f6151a5a5114719702730d1075643999
YoussefElogail <125933702+YoussefElogail@users.noreply.github.com>
2023-11-19 00:08:54 +0200

    fix error

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, json } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Summary from "./pages/Summary";
-import Incomes from "./pages/incomes";
+import Incomes from "./pages/Incomes";
 import Expenses from "./pages/expenses";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
